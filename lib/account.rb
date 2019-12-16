b38109e2d0000628e83aae285fb4a3a50031d165
Student <student@Makerss-MacBook-Pro-8.local>
2019-12-16 15:21:40 +0000

    addeded statement method that prints information and refactored tests

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,24 @@
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :activity
 
   def initialize
-    @balance = 0.00
+    @balance = 0
+    @activity = []
   end
 
   def deposit(amount)
     @balance += amount
+    @activity.unshift("#{Time.now.strftime("%d/%m/%Y")} || #{amount}.00 || || #{@balance}.00")
   end
 
   def withdraw(amount)
     @balance -= amount
+    @activity.unshift("#{Time.now.strftime("%d/%m/%Y")} || || #{amount}.00 || #{@balance}.00")
+  end
+
+  def statement
+    return "date || credit || debit || balance" + "\n" + "#{@activity.each {|x|}.join("\n")}" + "\n"
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,26 +3,39 @@ require 'account'
 describe Account do
   subject(:account) {described_class.new}
 
-  context 'starting balance' do
-    it 'should have zero as a starting balance' do
-      expect(subject.balance).to eq(0.00)
-    end
-  end
-
   context 'deposit' do
     it 'should be able to deposit and add to the existing balance' do
-      expect(subject.balance).to eq(0)
-      subject.deposit(1000.00)
-      expect(subject.balance).to eq(1000.00)
+      subject.deposit(1000)
+      expect(subject.balance).to eq(1000)
+    end
+
+    it 'should keep a log of the details when a deposit is made' do
+      subject.deposit(1000)
+      expect(subject.activity).to eq(["#{Time.now.strftime("%d/%m/%Y")} || 1000.00 || || 1000.00"])
     end
   end
 
   context 'withdraw' do
     it 'should be able to withdraw and subtract from the existing balance' do
-      expect(subject.balance).to eq(0)
-      subject.deposit(1000.00)
-      subject.withdraw(500.00)
-      expect(subject.balance).to eq(500.00)
+      subject.deposit(1000)
+      subject.withdraw(500)
+      expect(subject.balance).to eq(500)
+    end
+
+    it 'should keep a log of the date when a deposit and withdrawal is made' do
+      subject.deposit(1000)
+      subject.withdraw(500)
+      expect(subject.activity).to eq(["#{Time.now.strftime("%d/%m/%Y")} || || 500.00 || 500.00", "#{Time.now.strftime("%d/%m/%Y")} || 1000.00 || || 1000.00"])
+    end
+  end
+
+  context 'printing the statement' do
+    it 'should return the relevant informaton - formatted' do
+      subject.deposit(1000)
+      subject.withdraw(500)
+      expect(subject.statement).to include("date || credit || debit || balance")
+      expect(subject.statement).to include("#{Time.now.strftime("%d/%m/%Y")} || || 500.00 || 500.00")
+      expect(subject.statement).to include("#{Time.now.strftime("%d/%m/%Y")} || 1000.00 || || 1000.00")
     end
   end
 
