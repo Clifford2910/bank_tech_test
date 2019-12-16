c3e2dd3a5c74f8ef08992ec57263e901231358c7
Student <student@Makerss-MacBook-Pro-8.local>
2019-12-16 13:02:37 +0000

    class Account setup and passing a starting balance test

--- lib/account.rb
+++ lib/account.rb
@@ -1,1 +1,9 @@
+class Account
 
+  attr_reader :balance
+
+  def initialize
+    @balance = 0.00
+  end
+
+end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,1 +1,12 @@
+require 'account'
 
+describe Account do
+  subject(:account) {described_class.new}
+
+  context 'starting balance' do
+    it 'should have zero as a starting balance' do
+      expect(subject.balance).to eq(0.00)
+    end
+  end
+
+end
